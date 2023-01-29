a987443f0b783c40d79ede21666c3f4cbdae0c8c
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-01-29 16:28:15 +0300

    Add task2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Print the maximum of two numbers
+
+Console.WriteLine("Enter the first number:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+int b = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if (a == b)
+    Console.WriteLine("Numbers are equal: " + a + " and " + b);
+else
+{
+    if (a > b) max = a;
+    else max = b;
+    Console.WriteLine("The maximum number is " + max);
+}
