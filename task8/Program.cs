6e2a6aafeb5106a764bfd6c83a1ba67b99ee233d
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-01-29 17:06:00 +0300

    Add task8

---
+++ task8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Print all even numbers in range [1,N]
+
+Console.WriteLine("Enter the number:");
+int num = int.Parse(Console.ReadLine());
+int currnum = 2;
+
+if (num < 2) Console.WriteLine("Number " + num + " < 2. There are no even numbers in range [1,N].");
+else
+{
+    Console.WriteLine("Even numbers in range [1," + num + "]:");
+    while (currnum <= num)
+    {
+        Console.Write(currnum + ", ");
+        currnum += 2;
+    }
+}
