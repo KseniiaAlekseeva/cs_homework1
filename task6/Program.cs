b01b7674ea501be7094a850fb14f21529d8e3a4d
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-01-29 16:54:23 +0300

    Add task6

---
+++ task6/Program.cs
@@ -0,0 +1,7 @@
+﻿// Is the number even?
+
+Console.WriteLine("Enter the number:");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0) Console.WriteLine("Number " + a + " is even.");
+else Console.WriteLine("Number " + a + " is odd.");
